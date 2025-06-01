a30f1274770d48d890db36dd453f9f7f92744b3c
Aditya <73546596+0xC0ba1t@users.noreply.github.com>
2025-06-01 14:06:48 +0000

    Quartz sync: Jun 1, 2025, 2:06 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,6 +14,9 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
+          © {year} c0ba1t (Aditya Patil)
+        </p>
+        <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
